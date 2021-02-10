444665e2a49d2534448b56a524914b111d30e99c
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2021-02-10 15:44:30 +0100

    add a comment to illustrate the problem

--- src/main/org/example/QueryExample.kt
+++ src/main/org/example/QueryExample.kt
@@ -6,5 +6,5 @@ import org.example.generated.MySecondQuery
 fun main() {
     val bar1 = MyFirstQuery.Bar("qux")
     val bar2 = MySecondQuery.Bar("qux")
-    check(bar1::class == bar2::class)
+    check(bar1::class == bar2::class)  // fails, but how to make this work?
 }
